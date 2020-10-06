e2d53aaa8e810e281e2ba021a87d03af9ab86c9e
Colin Mackay <github@colinmackay.scot>
2020-10-06 23:26:14 +0100

    Set version number

--- Set-Version.ps1
+++ Set-Version.ps1
@@ -1,5 +1,38 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$false)]
+    [bool]
+    $IsPreview = $true
+)
+
+$VersionFile = "$PSScriptRoot/version.txt";
+
 Get-ChildItem Env:
 
-"STRAVAIG_PACKAGE_VERSION=1.2.3" | Out-File -FilePath $Env:GITHUB_ENV -Encoding UTF8 -Append
-"STRAVAIG_PACKAGE_VERSION_SUFFIX=some-suffix" | Out-File -FilePath $Env:GITHUB_ENV -Encoding UTF8 -Append
-
+$nextVersion = Get-Content $VersionFile -ErrorAction Stop
+if ($null -eq $nextVersion)
+{
+    Write-Error "The $VersionFile file is empty"
+    Exit 1
+}
+if ($nextVersion.GetType().BaseType.Name -eq "Array")
+{
+    $nextVersion = $nextVersion[0]
+    Write-Warning "$VersionFile contains more than one line of text. Using the first line."
+}
+if ($nextVersion -notmatch "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$")
+{
+    Write-Error "The contents of $VersionFile (`"$nextVersion`") not recognised as a valid version number."
+    Exit 2
+}
+
+$suffix = "~"
+
+if ($IsPreview)
+{
+    $suffix = "preview."
+    $suffix += $Env:GITHUB_RUN_NUMBER
+}
+
+"STRAVAIG_PACKAGE_VERSION=$nextVersion" | Out-File -FilePath $Env:GITHUB_ENV -Encoding UTF8 -Append
+"STRAVAIG_PACKAGE_VERSION_SUFFIX=suffix" | Out-File -FilePath $Env:GITHUB_ENV -Encoding UTF8 -Append
