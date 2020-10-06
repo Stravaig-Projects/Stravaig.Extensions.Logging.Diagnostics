70ab77bc830e07e9a3d80bc41621bc0b35c898b9
Colin Mackay <github@colinmackay.scot>
2020-10-06 23:12:50 +0100

    Interesting way to set an environment variable

--- Set-Version.ps1
+++ Set-Version.ps1
@@ -1,5 +1,5 @@
 Get-ChildItem Env:
 
-
-$Env:STRAVAIG_PACKAGE_VERSION = "1.2.3" #$nextVersion;
-$Env:STRAVAIG_PACKAGE_VERSION_SUFFIX = "some-suffix" #$suffix;
+Write-Output "::set-env name=STRAVAIG_PACKAGE_VERSION::1.2.3"
+Write-Output "::set-env name=STRAVAIG_PACKAGE_VERSION_SUFFIX=some-suffix"
+
