8e2bec0a23d1dc67db329fbf8f2e7a6aa1f5e583
Colin Mackay <github@colinmackay.scot>
2020-10-06 23:19:18 +0100

    New non-deprecated interesting way to set environment variables

--- Set-Version.ps1
+++ Set-Version.ps1
@@ -1,5 +1,5 @@
 Get-ChildItem Env:
 
-Write-Output "::set-env name=STRAVAIG_PACKAGE_VERSION::1.2.3"
-Write-Output "::set-env name=STRAVAIG_PACKAGE_VERSION_SUFFIX=some-suffix"
+"STRAVAIG_PACKAGE_VERSION=1.2.3" | Out-File -FilePath $Env:GITHUB_ENV -Encoding UTF8 -Append
+"STRAVAIG_PACKAGE_VERSION_SUFFIX=some-suffix" | Out-File -FilePath $Env:GITHUB_ENV -Encoding UTF8 -Append
 
