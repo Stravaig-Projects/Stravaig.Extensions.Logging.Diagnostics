8a365f275b80bd22949df7262b6f32b3ee45ee95
Colin Mackay <github@colinmackay.scot>
2020-10-07 21:09:57 +0100

    Update Set-Version.ps1

--- Set-Version.ps1
+++ Set-Version.ps1
@@ -2,13 +2,18 @@
 param (
     [Parameter(Mandatory=$false)]
     [bool]
-    $IsPreview = $true
+    $IsPreview = $true,
+
+    [Parameter(Mandatory=$false)]
+    [bool]
+    $IsPublicRelease = $false
 )
 
 $VersionFile = "$PSScriptRoot/version.txt";
 
 Get-ChildItem Env:
 
+# Work out the version number
 $nextVersion = Get-Content $VersionFile -ErrorAction Stop
 if ($null -eq $nextVersion)
 {
@@ -25,14 +30,22 @@ if ($nextVersion -notmatch "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$")
     Write-Error "The contents of $VersionFile (`"$nextVersion`") not recognised as a valid version number."
     Exit 2
 }
+"STRAVAIG_PACKAGE_VERSION=$nextVersion" | Out-File -FilePath $Env:GITHUB_ENV -Encoding UTF8 -Append
 
+# Work out the suffix (~ = no suffix)
 $suffix = "~"
-
 if ($IsPreview)
 {
     $suffix = "preview."
     $suffix += $Env:GITHUB_RUN_NUMBER
 }
+"STRAVAIG_PACKAGE_VERSION_SUFFIX=$suffix" | Out-File -FilePath $Env:GITHUB_ENV -Encoding UTF8 -Append
 
-"STRAVAIG_PACKAGE_VERSION=$nextVersion" | Out-File -FilePath $Env:GITHUB_ENV -Encoding UTF8 -Append
-"STRAVAIG_PACKAGE_VERSION_SUFFIX=suffix" | Out-File -FilePath $Env:GITHUB_ENV -Encoding UTF8 -Append
+if ($IsPublicRelease)
+{
+    "STRAVAIG_PUBLISH_TO_NUGET=true" | Out-File -FilePath $Env:GITHUB_ENV -Encoding UTF8 -Append
+}
+else 
+{
+    "STRAVAIG_PUBLISH_TO_NUGET=false" | Out-File -FilePath $Env:GITHUB_ENV -Encoding UTF8 -Append
+}
