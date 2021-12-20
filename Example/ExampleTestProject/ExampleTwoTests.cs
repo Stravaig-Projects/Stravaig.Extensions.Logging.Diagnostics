0844fc20d834d9a584c76f5453c15714afdba98d
Colin Mackay <github@colinmackay.scot>
2021-12-20 11:08:14 +0000

    (#60) Add the Renderer to the example project

--- Example/ExampleTestProject/ExampleTwoTests.cs
+++ Example/ExampleTestProject/ExampleTwoTests.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 using Shouldly;
 using Stravaig.Extensions.Logging.Diagnostics;
+using Stravaig.Extensions.Logging.Diagnostics.Render;
 
 namespace ExampleTestProject
 {
@@ -18,6 +19,7 @@ namespace ExampleTestProject
 
             public void DoStuff()
             {
+                _logger.LogInformation("About to do some work.");
                 // Do whatever the service needs to do.
                 _logger.LogInformation("The work is done.");
             }
@@ -26,21 +28,36 @@ namespace ExampleTestProject
         [Test]
         public void TestServiceLoggerCalled()
         {
+            // ****************************************
             // Arrange
+            
+            // Set up the logger factory with a test capture logger
             var logProvider = new TestCaptureLoggerProvider();
             var factory = LoggerFactory.Create(builder =>
             {
                 builder.AddProvider(logProvider);
             });
+            
+            // Inject the factory into the service.
             var service = new MyService(factory);
 
+            // ****************************************
             // Act
             service.DoStuff();
 
+            // ****************************************
             // Assert
+            
+            // Retrieve the logs for the MyService source
             var logs = logProvider.GetLogEntriesFor<MyService>();
-            logs.Count.ShouldBe(1);
-            logs[0].FormattedMessage.ShouldContain("The work is done.");
+            
+            // Render the logs to the console
+            logs.RenderLogs(Formatter.SimpleByLocalTime, Sink.Console);
+            
+            // Assert that the logs are as they should be
+            logs.Count.ShouldBe(2);
+            logs[0].FormattedMessage.ShouldContain("About to do some work.");
+            logs[1].FormattedMessage.ShouldContain("The work is done.");
         }
 
     }
