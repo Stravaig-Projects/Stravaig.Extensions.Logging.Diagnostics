80e4e97531ba13c1f9dc03cbad7ec28bf9356724
Colin Mackay <github@colinmackay.scot>
2022-12-11 10:46:32 +0000

    (#115) Fix issues raised

--- src/Stravaig.Extensions.Logging.Diagnostics.Tests/Net7/Helpers/FileWithTwoServices.cs
+++ src/Stravaig.Extensions.Logging.Diagnostics.Tests/Net7/Helpers/FileWithTwoServices.cs
@@ -1,14 +1,13 @@
-﻿using Microsoft.Extensions.Logging;
+﻿#if NET7_0_OR_GREATER
+using Microsoft.Extensions.Logging;
 
 namespace Stravaig.Extensions.Logging.Diagnostics.Tests.Net7.Helpers;
-
-#if NET7_0_OR_GREATER
 
 public class SomeOpenService
 {
     public SomeOpenService(ILoggerFactory logFactory)
     {
-        var fileService = new SomeFileScopedService(logFactory);
+        var _ = new SomeFileScopedService(logFactory);
     }
 }
 
