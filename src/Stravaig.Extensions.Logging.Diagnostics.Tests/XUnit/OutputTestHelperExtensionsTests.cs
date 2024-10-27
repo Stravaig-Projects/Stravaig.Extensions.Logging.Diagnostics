4ddb8a41b6e907ed5c7ff4dd763a56f138f4fed6
Colin Mackay <github@colinmackay.scot>
2024-10-27 09:56:46 +0000

    (#170) Add extension methods to write output from logger or provider

--- src/Stravaig.Extensions.Logging.Diagnostics.Tests/XUnit/OutputTestHelperExtensionsTests.cs
+++ src/Stravaig.Extensions.Logging.Diagnostics.Tests/XUnit/OutputTestHelperExtensionsTests.cs
@@ -26,7 +26,7 @@ public class OutputTestHelperExtensionTests
     }
 
     [Test]
-    public void RenderedLogMessagesGetSentToOutputTestHelper()
+    public void RenderedLogEntryMessagesSentToOutputTestHelper()
     {
         ResetLogSequence();
         var outputHelper = new OutputHelper();
@@ -36,9 +36,45 @@ public class OutputTestHelperExtensionTests
 
         var logs = logger.GetLogs();
         outputHelper.WriteLogs(logs);
-        
+
         outputHelper.Messages[0].ShouldBe("[0 Information Stravaig.Extensions.Logging.Diagnostics.Tests.XUnit.OutputTestHelperExtensionTests] This is an information Message.");
         outputHelper.Messages[1].ShouldBe("[1 Information Stravaig.Extensions.Logging.Diagnostics.Tests.XUnit.OutputTestHelperExtensionTests] This is a Warning Message.");
+    }
+
+    [Test]
+    public void RenderedLoggerSentToOutputTestHelper()
+    {
+        ResetLogSequence();
+        var outputHelper = new OutputHelper();
+        var logger = new TestCaptureLogger<OutputTestHelperExtensionTests>();
+        logger.LogInformation("This is an information Message.");
+        logger.LogInformation("This is a Warning Message.");
+
+        outputHelper.WriteLogs(logger);
+
+        outputHelper.Messages[0].ShouldBe("[0 Information Stravaig.Extensions.Logging.Diagnostics.Tests.XUnit.OutputTestHelperExtensionTests] This is an information Message.");
+        outputHelper.Messages[1].ShouldBe("[1 Information Stravaig.Extensions.Logging.Diagnostics.Tests.XUnit.OutputTestHelperExtensionTests] This is a Warning Message.");
+    }
+
+    [Test]
+    public void RenderedLoggerProviderSentToOutputTestHelper()
+    {
+        ResetLogSequence();
+        var outputHelper = new OutputHelper();
+        var loggerProvider = new TestCaptureLoggerProvider();
+        var logger1 = loggerProvider.CreateLogger("logger1");
+        var logger2 = loggerProvider.CreateLogger("logger2");
+        logger1.LogInformation("This is an information Message on the first logger.");
+        logger2.LogInformation("This is an information Message on the second logger.");
+        logger2.LogWarning("This is a Warning Message on the second logger.");
+        logger1.LogWarning("This is a Warning Message on the first logger.");
+
+        outputHelper.WriteLogs(loggerProvider);
+
+        outputHelper.Messages[0].ShouldBe("[0 Information logger1] This is an information Message on the first logger.");
+        outputHelper.Messages[1].ShouldBe("[1 Information logger2] This is an information Message on the second logger.");
+        outputHelper.Messages[2].ShouldBe("[2 Warning logger2] This is a Warning Message on the second logger.");
+        outputHelper.Messages[3].ShouldBe("[3 Warning logger1] This is a Warning Message on the first logger.");
     }
 
     private void ResetLogSequence()
