8f3f07d4b367c810152d1b2af695b5a4a70e3030
Colin Mackay <colinmackay@Colins-Mac-mini.local>
2026-03-03 23:10:30 +0000

    (#218) Fix rebase issues
    # Conflicts:
    #	release-notes/wip-release-notes.md

--- src/Stravaig.Extensions.Logging.Diagnostics.XUnit.v3.Tests/OutputTestHelperExtensionsTests.cs
+++ src/Stravaig.Extensions.Logging.Diagnostics.XUnit.v3.Tests/OutputTestHelperExtensionsTests.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 using Shouldly;
+using Stravaig.Extensions.Logging.Diagnostics.Tests.Helpers;
 using Xunit;
 
 namespace Stravaig.Extensions.Logging.Diagnostics.XUnit.v3.Tests;
@@ -89,14 +90,5 @@ public class OutputTestHelperExtensionTests
         outputHelper.Messages[1].ShouldBe("[1 Information logger2] This is an information Message on the second logger.");
         outputHelper.Messages[2].ShouldBe("[2 Warning logger2] This is a Warning Message on the second logger.");
         outputHelper.Messages[3].ShouldBe("[3 Warning logger1] This is a Warning Message on the first logger.");
-    }
-
-    private void ResetLogSequence()
-    {
-        var logEntryType = typeof(LogEntry);
-        var field = logEntryType.GetField("_sequence", BindingFlags.Static | BindingFlags.NonPublic);
-        if (field == null)
-            throw new InvalidOperationException("Expected LogEntry to have a private field called _sequence.");
-        field.SetValue(null, 0);
     }
 }
